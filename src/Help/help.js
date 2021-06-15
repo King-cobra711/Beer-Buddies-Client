f9724c81bd766206a066cf99ccfc0a6aac9b4d47
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2021-06-15 14:06:30 +1000

    help fixes

--- src/Help/help.js
+++ src/Help/help.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-import classes from "./help.module.css";
+// import classes from "./help.module.css";
 
 import { useHistory } from "react-router-dom";
 
@@ -11,13 +11,15 @@ const Help = () => {
   return (
     <div style={{ color: "black", backgroundColor: "white" }}>
       <button onClick={() => home()}>Main Menu</button>
-      <h1>Beer Buddies Navigation guide</h1>
-      <h2>Main menu</h2>
-      <p>
+      <h1 style={{ color: "black", backgroundColor: "white" }}>
+        Beer Buddies Navigation guide
+      </h1>
+      <h2 style={{ color: "black", backgroundColor: "white" }}>Main menu</h2>
+      <p style={{ color: "black", backgroundColor: "white" }}>
         You do not have to be logged in to play the easy (tipsy) game mode.
         However, in order to:
       </p>
-      <ul>
+      <ul style={{ color: "black", backgroundColor: "white" }}>
         <li>play higher difficulty modes</li>
         <li>add friends</li>
         <li>have a profile page</li>
@@ -25,14 +27,18 @@ const Help = () => {
       You must register an account/log in. Click play to go to choose difficulty
       page. Click login to go to login/register page. click leaderboards to veiw
       current leaderboards of registered users.
-      <h2>Choose difficulty</h2>
-      <ul>
+      <h2 style={{ color: "black", backgroundColor: "white" }}>
+        Choose difficulty
+      </h2>
+      <ul style={{ color: "black", backgroundColor: "white" }}>
         <li>Tipsy = Easy. 2 x 3 squares</li>
         <li>Drunk = Medium. 2 x 4 squares</li>
         <li>Hammered = Easy. 3 x 4 squares</li>
       </ul>
-      <h2>Playing the game</h2>
-      <p>
+      <h2 style={{ color: "black", backgroundColor: "white" }}>
+        Playing the game
+      </h2>
+      <p style={{ color: "black", backgroundColor: "white" }}>
         The game will start when the first card is clicked. Once clicked the
         timer will start and you must match all the cards as quick as you can.
         The timer will stop once the last pair of cards is matched. If you are
@@ -40,16 +46,18 @@ const Help = () => {
         but not saved. To reset click the reset button and the timer will return
         to zero seconds and the cards will be flipped and shuffled.
       </p>
-      <h2>Registered Users</h2>
-      <p>
+      <h2 style={{ color: "black", backgroundColor: "white" }}>
+        Registered Users
+      </h2>
+      <p style={{ color: "black", backgroundColor: "white" }}>
         Once registered/logged in a welcome message will be displayed with your
         username under the beerbuddies header. Click the user icon next to the
         right of your username to access your usercard or to log-out. You are
         also now able to access the higher difficulties however you must
         complete the previous difficulty in the qualifying time.
       </p>
-      <h2>Usercard</h2>
-      <p>
+      <h2 style={{ color: "black", backgroundColor: "white" }}>Usercard</h2>
+      <p style={{ color: "black", backgroundColor: "white" }}>
         When veiwing your usercard you can see details about your account. These
         include Level, Biography, scores etc. To edit you user details you can
         click the "cog/gear" icon to the top right. To add friends you can click
