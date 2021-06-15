dfe7686cfe25e1826b7e71b2d1e4133161e6ba13
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2021-06-15 13:19:05 +1000

    help fixes

--- src/Help/help.js
+++ src/Help/help.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-// import classes from "./help.module.css";
+import classes from "./help.module.css";
 
 import { useHistory } from "react-router-dom";
 
